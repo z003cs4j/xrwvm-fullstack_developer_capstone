22aaaed0671b0372324c6be0cccabbde19d777b1
Tang <tszling.tang@siemens.com>
2025-06-23 23:41:49 -0400

    Fix settings.py urls.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://elainetltang-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://elainetltang-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 'https://elainetltang-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',
+                 'https://elainetltang-8000.theianext-0-labs-'
+                 'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://elainetltang-8000.theianext-0-labs'
+                        '-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                        'https://elainetltang-8000.theiadockernext-1-lab'
+                        's-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 REST_FRAMEWORK = {
