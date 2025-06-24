65111b7169bf689b0b7876f567ea578822a7d70e
Tang <tszling.tang@siemens.com>
2025-06-23 23:21:14 -0400

    Fix restapis.py 2

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,7 +29,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred {e}")
 
 
 # def analyze_review_sentiments(text):
@@ -55,4 +55,4 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except Exception as err:
-        print("Network exception occurred")
+        print(f"Network exception occurred {err}")
