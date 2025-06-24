05df6278d6fcd6c78ef2432c4a26525006a0841f
Tang <tszling.tang@siemens.com>
2025-06-23 23:39:23 -0400

    fix views.py 7

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -17,7 +17,7 @@ from .populate import initiate
 
 from .models import CarMake, CarModel
 
-#from .restapis import get_request, analyze_review_sentiments, post_review
+# from .restapis import get_request, analyze_review_sentiments, post_review
 from .restapis import get_request, analyze_review_sentiments
 
 # Get an instance of a logger
