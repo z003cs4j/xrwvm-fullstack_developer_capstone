cd6fa8c31c17277afb5b7c83ec9ae4ace271c282
Tang <tszling.tang@siemens.com>
2025-06-23 23:15:08 -0400

    Fix populate.py 3

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,8 +12,8 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-                                                         description=data['description']))
+        thisObj = CarMake.objects.create(name=data['name'],description=data['description'])
+        car_make_instances.append(thisObj)
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -51,4 +51,5 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
+                                type=data['type'], year=data['year'])
